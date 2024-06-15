c945cce094e52b20c1f34ed7a4bf41b236c7b34f
Alex_Kar <alexkar0706@gmail.com>
2024-06-15 10:23:37 +0300

    [+] Add scan port feature to detect active COM ports

--- ComPort/ComPort.c
+++ ComPort/ComPort.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
 #include <windows.h>
+
+#pragma comment (lib, "OneCore.lib")
 
 void PrintError(const char* context)
 {
@@ -107,11 +112,99 @@ SSIZE_T ReadPort(HANDLE port, uint8_t* buffer, size_t size)
 	return received;
 }
 
+void WaitAvailablePort(ULONG* availablePorts, ULONG portsMax, ULONG* availablePortsNum)
+{
+	assert(availablePorts != NULL && availablePortsNum != NULL);
+
+	GetCommPorts(availablePorts, portsMax, availablePortsNum);
+
+	uint8_t animation = 0;
+	while (*availablePortsNum == 0) {
+
+		if (animation == 0) printf("Waiting for the COM ports");
+		else if (animation >= 1 && animation <= 3) printf(".");
+
+		GetCommPorts(availablePorts, portsMax, availablePortsNum);
+
+		Sleep(500);
+		animation++;
+		if (animation > 3) {
+			animation = 0;
+			system("cls");
+		}
+
+	}
+
+	system("cls");
+}
+
+// Print available ports to the console
+void PrintPorts(ULONG* availablePorts, ULONG availablePortsNum)
+{
+	assert(availablePorts != NULL);
+
+	printf("Available ports:\n");
+
+	for (ULONG portNum = 0; portNum < availablePortsNum; portNum++) {
+	
+		printf("COM%lu\n", availablePorts[portNum]);
+
+	}
+
+	printf("\n");
+}
+
+void RequestNewDevice(char* deviceName, size_t nameSizeMax) 
+{
+	while (1) {
+
+		char InputBuffer[256] = "\\\\.\\";
+		printf("Type device name: ");
+		
+		if (fgets(&InputBuffer[4], sizeof(InputBuffer) - 4, stdin) == NULL) continue;
+
+
+		if ((strncmp("COM", &InputBuffer[4], 3) != 0) || 
+			(!isdigit(InputBuffer[7])) || 
+			(InputBuffer[8] != '\n' && !isdigit(InputBuffer[8])) ||
+			(InputBuffer[9] != '\n' &&  isdigit(InputBuffer[8]))) {
+
+			printf("Wrong device name\n");
+			continue;
+
+		}
+
+		size_t digitNum = 1;
+		if (isdigit(InputBuffer[8])) digitNum = 2;
+		InputBuffer[7 + digitNum] = '\0';
+		strncpy_s(deviceName, nameSizeMax, InputBuffer, (8 + digitNum));
+		return;
+	}
+}
+
 int main(void)
 {
-	HANDLE port = OpenSerialPort("\\\\.\\COM5", NULL, NULL);
-	if (port == INVALID_HANDLE_VALUE) { return 1; }
+	ULONG availablePortsNum;
+	ULONG availablePorts[50];
 
-	CloseHandle(port);
+	while (1) {
+		char deviceName[20] = "";
+
+		WaitAvailablePort(availablePorts, sizeof(availablePorts) / sizeof(ULONG), &availablePortsNum);
+		PrintPorts(availablePorts, availablePortsNum);
+		RequestNewDevice(deviceName, sizeof(deviceName));
+		HANDLE port = OpenSerialPort(deviceName, NULL, NULL);
+		if (port == INVALID_HANDLE_VALUE) {
+			Sleep(1500);
+			system("cls");
+			continue;
+		}
+		printf("Device opened. Start reading COM port:\n\n");
+
+
+
+		CloseHandle(port);
+		system("cls");
+	}
 	return 0;
 }
